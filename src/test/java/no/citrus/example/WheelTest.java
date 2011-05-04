d3f50327bdfe9bbdf6ec6482cbd5b02ae3bf2008
Simon Remøy <simon.remoy@gmail.com>
2011-05-04 12:50:14 +0200

    changed wheeltest

--- src/test/java/no/citrus/example/WheelTest.java
+++ src/test/java/no/citrus/example/WheelTest.java
@@ -6,20 +6,20 @@ import static org.junit.Assert.assertThat;
 
 public class WheelTest {
 	
-//	@Test
-//	public void wheel_should_spin(){
-//		Tyre tyre = new Tyre();
-//		Wheel wheel = new Wheel(tyre);
-//		boolean spin = wheel.spin();
-//		assertThat(spin, equalTo(true));
-//	}
-//	
-//	@Test
-//	public void wheel_should_have_inflated_tyre(){
-//		Tyre tyre = new Tyre();
-//		Wheel wheel = new Wheel(tyre);
-//		Tyre fetchedTyre = wheel.getTyre();
-//		boolean inflated = fetchedTyre.isInflated();
-//		assertThat(inflated, equalTo(true));
-//	}
+	@Test
+	public void wheel_should_spin(){
+		Tyre tyre = new Tyre();
+		Wheel wheel = new Wheel(tyre);
+		boolean spin = wheel.spin();
+		assertThat(spin, equalTo(true));
+	}
+	
+	@Test
+	public void wheel_should_have_inflated_tyre(){
+		Tyre tyre = new Tyre();
+		Wheel wheel = new Wheel(tyre);
+		Tyre fetchedTyre = wheel.getTyre();
+		boolean inflated = fetchedTyre.isInflated();
+		assertThat(inflated, equalTo(true));
+	}
 }
